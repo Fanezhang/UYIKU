61d78819c7d1c8d3560149bc50b065894a772da3
dd5407 <476700530@qq.com>
2017-04-10 19:24:02 +0800

    新增爬虫测试用例 	爬商品详情页 		标题	销量	商品属性

--- uyikuJsoup/src/main/java/test/jsoupTest.java
+++ uyikuJsoup/src/main/java/test/jsoupTest.java
@@ -2,19 +2,25 @@ package test;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
 
 import org.apache.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class jsoupTest {
 	@Resource
@@ -62,4 +68,100 @@ public class jsoupTest {
 			bw.close();
 			
 	}
+	@Test
+	public void test3(){
+		int count=1;
+		String url="https://www.taobao.com/markets/nvzhuang/taobaonvzhuang";
+		Document doc=null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Elements link = doc.select("dd a");
+		for (Element element : link) {
+			String href = element.attr("href");
+			String text = element.text();
+			if(href.contains("javascript")){
+				continue;
+			}
+//			System.out.println(count+++"."+text+":"+href);
+			System.out.println(count+" "+text+":"+href);
+			//https://tmatch.simba.taobao.com/?name=tbuad&o=j&pid=420434_1006&count=12&keyword=&p4p=tbcc_p4p_c2015_8_130027_14918041125041491804116091&catid=1623&se=3aba140c15b8ae4295cea246d7325fda
+			//第二层遍历
+			Document doc2 =null;
+			int count2=1;
+			try {
+				doc2= Jsoup.connect(href).get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Elements link2 = doc2.select("script");
+			for (Element element2 : link2) {
+				String src2 = element2.attr("src");
+				
+				System.out.println(src2);
+				
+//				String href2 = element2.attr("href");
+//				String text2 = element2.text();
+//				if(href2.contains("javascript")){
+//					continue;
+//				}
+//				System.out.println("\t"+count+"."+count2+" "+text2+":");
+//				count2++;
+			}
+			count++; 
+		}
+	}
+	@Test
+	public void test4() {
+		String url="https://tmatch.simba.taobao.com/?name=tbuad&o=j&pid=420434_1006&count=12&keyword=&p4p=tbcc_p4p_c2015_8_130027_14918041125041491804116091&catid=1624&se=3aba140c15b8ae4295cea246d7325fda";
+		
+			Connection connect = Jsoup.connect(url);
+			try {
+				Response re = connect.ignoreContentType(true).execute();
+				String body = re.body();
+				//把jsonp格式转为json格式
+				int index = body.indexOf("[");
+				int last = body.lastIndexOf("]");
+				String substring = body.substring(index+1, last);
+				//分割json
+				String[] split = substring.split("},");
+				for (String string : split) {//遍历分割出来的数组
+					string=string+"}";
+					if(string.contains("}}")){//去掉最后一条数据多余的右括号
+						string=string.substring(0,string.length()-1);
+					}
+					//把json转为map
+					ObjectMapper om=new ObjectMapper();
+//					JsonNode r = om.readTree(string);
+//					JsonNode jsonNode = r.get("EURL");
+//					System.out.println(jsonNode.toString());
+					Map<String,String> map=om.readValue(string,Map.class);
+					String itemUrl = map.get("EURL");
+					System.out.println(itemUrl);
+					//第二层 商品详情页
+					Document doc = Jsoup.connect(itemUrl).get();
+					Elements eles = doc.select("#J_Title h3");
+					if(eles.toString().equals("")){
+//						eles=doc.select("div[class=tb-detail-hd] h1");
+//						eles=doc.select("span[class=tm-count]");销量
+//						eles=doc.select("#J_deliveryAdd");todo
+						eles=doc.select("#J_AttrUL li");
+					}
+					for (Element element : eles) {
+						String text = element.text();
+						System.out.println("\t"+text);
+					}
+//					System.out.println(string);
+				}
+
+//				System.out.println(substring);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	
 }
